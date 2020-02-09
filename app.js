062a0c52acd41f7e86c5c04e1a1202cde70903e8
lucas <lucas180395@gmail.com>
2020-02-09 17:26:12 +0100

    ah

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-console.log("Hello!")
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-ui.start('#firebaseui-auth-container', {
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  // Other config options...
+console.log("hello")
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyA17BX2p9pTUnQz8fWZL7av7FV9umRflgU',
+  authDomain: 'buyo-4545o.firebaseapp.com',
+  projectId: 'buyo-4545o'
+});
+
+var db = firebase.firestore();
+
+db.collection("cats").add({
+  id: 5,
+  name: "Lovelace"
+})
+.then(function(docRef) {
+  console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+  console.error("Error adding document: ", error);
 });
